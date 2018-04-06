f482cec633566c90a9ceefb0b05173b76a375232
piotr <piotr.pilip@gmail.com>
2018-04-06 17:21:48 +0200

    Deleted manifesto

--- src/master_database_pusher/mdp_database_queries.py
+++ src/master_database_pusher/mdp_database_queries.py
@@ -1,4 +1,26 @@
 from configparser import ConfigParser
+from cassandra.cluster import Cluster
 
-cfg = ConfigParser
+
+cfg = ConfigParser()
 cfg.read('../../main.conf')
+
+
+def insert_data(columns,data,table):
+    cluster = Cluster()
+    session = cluster.connect('tutorial')
+
+    query = "INSERT INTO {} (".format(table)
+    for col in columns:
+        query+=' {},'.format(col)
+    query=query[:-1]
+    query+=') VALUES ('
+    for col in columns:
+        query+=' %s,'.format(col)
+    query=query[:-1]+')'
+    print(query)
+
+
+
+if __name__ == '__main__':
+    insert_data(['lol','d'],['xd',1],'kwejk')
--- test/cassandraTest.py
+++ test/cassandraTest.py
@@ -5,3 +5,8 @@ session = cluster.connect('tutorial')
 rows = session.execute('SELECT * FROM dupa')
 for r in rows:
     print(r)
+
+
+number = 2
+text = 'Hi'
+rows = session.execute( 'INSERT INTO dupa (id_dupa,name) VALUES (%s,%s)',(number,text,)  )
